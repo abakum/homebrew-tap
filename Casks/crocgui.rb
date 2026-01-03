80b504d793029f52a7965bd805063745ca34c906
abakum <koka.abakum@gmail.com>
2026-01-03 18:55:33 +0300

    e

--- Casks/crocgui.rb
+++ Casks/crocgui.rb
@@ -8,8 +8,7 @@ cask "crocgui" do
   sha256 arm:   "b90e67de3413ccbad2624b7b833215233a9ad38c8eaea682c886496e63392865",
          intel: "4839530f83755f7759fd0de26ca43c6d5b9154e7e2cb8b4f47a6eedc0c8495ed"
 
-  url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-#{arch}.dmg",
-      verified: "github.com/abakum/crocgui/"
+  url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-#{arch}.dmg"
   name "crocgui"
   desc "GUI for croc — secure file transfer tool"
   homepage "https://github.com/abakum/crocgui"
