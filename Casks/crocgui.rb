9429df56f84bbc06ad0203eff2ad8054c8a237c6
abakum <koka.abakum@gmail.com>
2026-01-03 15:55:28 +0300

    crocgui

--- Casks/crocgui.rb
+++ Casks/crocgui.rb
@@ -4,15 +4,21 @@
 cask "crocgui" do
   version "1.11.31"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "4839530f83755f7759fd0de26ca43c6d5b9154e7e2cb8b4f47a6eedc0c8495ed"
+
     url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-amd64.dmg",
         verified: "github.com/abakum/crocgui/"
+
     app "crocgui-amd64.app"
-  else
+  end
+
+  on_arch arm: :arm64 do
     sha256 "b90e67de3413ccbad2624b7b833215233a9ad38c8eaea682c886496e63392865"
+
     url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-arm64.dmg",
         verified: "github.com/abakum/crocgui/"
+
     app "crocgui-arm64.app"
   end
 
@@ -22,6 +28,5 @@ cask "crocgui" do
 
   depends_on macos: ">= :big_sur"
 
-  zap trash: [
-  ]
+  zap trash: []
 end
