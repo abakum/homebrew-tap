ba61898185655bd520d0d61a953d414e6862cb9c
abakum <koka.abakum@gmail.com>
2026-01-03 18:02:52 +0300

    a

--- Casks/crocgui.rb
+++ Casks/crocgui.rb
@@ -2,29 +2,24 @@
 # frozen_string_literal: true
 
 cask "crocgui" do
+  arch arm: "arm64", intel: "amd64"
+
   version "1.11.31"
+  sha256 arm:   "b90e67de3413ccbad2624b7b833215233a9ad38c8eaea682c886496e63392865",
+         intel: "4839530f83755f7759fd0de26ca43c6d5b9154e7e2cb8b4f47a6eedc0c8495ed"
 
-  on intel do
-    sha256 "4839530f83755f7759fd0de26ca43c6d5b9154e7e2cb8b4f47a6eedc0c8495ed"
-
-    url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-amd64.dmg",
-        verified: "github.com/abakum/crocgui/"
-    app "crocgui-amd64.app"
-  end
-
-  on arch arm: :arm64 do
-    sha256 "b90e67de3413ccbad2624b7b833215233a9ad38c8eaea682c886496e63392865"
-
-    url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-arm64.dmg",
-        verified: "github.com/abakum/crocgui/"
-    app "crocgui-arm64.app"
-  end
-
+  url "https://github.com/abakum/crocgui/releases/download/v#{version}/crocgui-#{arch}.dmg",
+      verified: "github.com/abakum/crocgui/"
   name "crocgui"
   desc "GUI for croc — secure file transfer tool"
   homepage "https://github.com/abakum/crocgui"
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :sierra"
 
-  zap trash: []
+  app "crocgui.app"
+
+  zap trash: [
+    "~/Library/Application Support/crocgui",
+    "~/Library/Preferences/com.crocgui.plist",
+  ]
 end
