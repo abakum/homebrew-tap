342364cc1e7654337366dcbc484f1d5660e23d02
abakum <koka.abakum@gmail.com>
2026-01-04 02:49:38 +0300

    Update crocgui.rb

--- Casks/crocgui.rb
+++ Casks/crocgui.rb
@@ -6,7 +6,7 @@ cask "crocgui" do
 
   version "1.11.32"
   sha256 arm:          "cb8aae92631feae210b8f542db5fe564fa2ec454ee83acc3626a619696c0646c",
-         intel:        "4839530f83755f7759fd0de26ca43c6d5b9154e7e2cb8b4f47a6eedc0c8495ed",
+         intel:        "908b388826472dd7d944810754f8ad4e136f25edafc9b88a82e677047c6e902a",
          arm64_linux:  "0",
          x86_64_linux: "0"
 
