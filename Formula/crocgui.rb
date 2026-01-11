7ade055c9b6671a9ef7bdf9ea9e4f0083ead5d94
abakum <koka.abakum@gmail.com>
2026-01-11 03:13:00 +0300

    post_install

--- Formula/crocgui.rb
+++ Formula/crocgui.rb
@@ -5,8 +5,6 @@ class Crocgui < Formula
   sha256 "b0f13da18ff4339d837d69a83f4fe591e6cd2ee3344c676d2298bf223e5bbdc4"
   license "ISC"
 
-  APP_ID = "com.github.howeyc.crocgui".freeze
-
   livecheck do
     url :url
     strategy :github_latest
@@ -22,23 +20,7 @@ class Crocgui < Formula
   end
 
   def post_install
-    local_apps_dir = Pathname(Dir.home)/".local/share/applications"
-    local_apps_dir.mkpath
-
-    desktop_file = prefix/"share/applications/#{APP_ID}.desktop"
-    if desktop_file.exist?
-      (local_apps_dir/"#{APP_ID}.desktop").make_symlink(desktop_file)
-      ohai "Desktop entry linked to #{local_apps_dir}"
-    end
-  end
-
-  def post_uninstall
-    local_desktop_file = Pathname(Dir.home)/".local/share/applications/#{APP_ID}.desktop"
-
-    if local_desktop_file.symlink?
-      local_desktop_file.delete
-      ohai "Removed desktop entry: #{local_desktop_file}"
-    end
+    quiet_system "update-desktop-database", HOMEBREW_PREFIX/"share/applications" if which("update-desktop-database")
   end
 
   test do
