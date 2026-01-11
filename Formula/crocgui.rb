d018de6c60d3d6a76873499f6d00dfcc3af49420
abakum <koka.abakum@gmail.com>
2026-01-11 12:01:10 +0300

    caveats

--- Formula/crocgui.rb
+++ Formula/crocgui.rb
@@ -5,6 +5,8 @@ class Crocgui < Formula
   sha256 "b0f13da18ff4339d837d69a83f4fe591e6cd2ee3344c676d2298bf223e5bbdc4"
   license "ISC"
 
+  APP_ID = "com.github.howeyc.crocgui".freeze
+
   livecheck do
     url :url
     strategy :github_latest
@@ -21,6 +23,31 @@ class Crocgui < Formula
 
   def post_install
     quiet_system "update-desktop-database", HOMEBREW_PREFIX/"share/applications" if which("update-desktop-database")
+  end
+
+  def caveats
+    line_to_add = "export XDG_DATA_DIRS=\"#{HOMEBREW_PREFIX}/share:$XDG_DATA_DIRS\""
+    bashrc = Pathname.new(Dir.home)/".bashrc"
+    
+    # Base message that always shows
+    msg = "You can launch the application from your menu or via terminal:\n  gtk-launch #{APP_ID}"
+
+    # Check if we need to add the environment setup instructions
+    if bashrc.exist? && bashrc.read.include?(line_to_add)
+      msg
+    else
+      <<~EOS
+        The desktop shortcut was installed, but Homebrew's share directory is not in your XDG_DATA_DIRS.
+        To see the application in your system menu and use gtk-launch, please run:
+
+        grep -qF '#{line_to_add}' ~/.bashrc || echo '#{line_to_add}' >> ~/.bashrc
+
+        Then, refresh your shell environment:
+          source ~/.bashrc
+
+        #{msg}
+      EOS
+    end
   end
 
   test do
